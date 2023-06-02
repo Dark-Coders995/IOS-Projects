c1b90862232b92cf6b7f79b74539be750cc43b26
Ayush Gupta <59391849+Dark-Coders995@users.noreply.github.com>
2023-06-02 10:28:50 +0530

    Added

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ayush Gupta on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
